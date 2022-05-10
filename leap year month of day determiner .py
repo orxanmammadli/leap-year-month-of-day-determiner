d9b95a46c30ae719bbbec4dffcfb5b2556661d21
Orkhan Mammadli <63901366+orxanmammadli@users.noreply.github.com>
2022-05-10 09:44:14 +0200

    programme code logic changed

--- leap year month of day determiner .py
+++ leap year month of day determiner .py
@@ -12,15 +12,10 @@ def is_leap(year):
 
 def days_in_month(year, month):
   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
-  if not is_leap(year):
-    print(month_days[month - 1])
-  elif is_leap(year):
-    if month==2:
-      print(29)
-    else:
-      print(month_days[month-1])
+  if is_leap(year) and month==2:
+    return 29
+  return month_days[month-1]
   
-
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
